95e5ba079aaf3ec4a9d52e8f8bfbec5d7cb97e66
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-14 15:51:40 -0500

    slightly better linked list

--- machine_learning/language_model.py
+++ machine_learning/language_model.py
@@ -6,14 +6,25 @@ sentences = []
 
 content = content.split('. ')
 
-class LinkedList:
+class Node:
     def __init__(self):
         self.value = None
         self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
     
+model = LinkedList()
 
 for sentence in content:
-    print(sentence)
+    if model.value == None:
+        model.value = sentence
+    current = model.next
+    while current != None:
+        current = current.next
+
+print(model.value)
 
 
 f.close()
